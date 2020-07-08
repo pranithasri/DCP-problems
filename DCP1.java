17fb966d35b2b15b6e18e6356d683ffa79ad3d32
pranithasri <pranithasai.e@gmail.com>
2020-07-08 16:29:57 +0530

    First DCP

---
+++ DCP1.java
@@ -0,0 +1,37 @@
+Given a list of numbers nums and a number k, return whether any two numbers from the list add up to k. You may not use the same element twice.
+
+Note: Numbers can be negative or 0.
+
+Bonus: Can you do this in one pass?
+
+
+
+import java.util.*;
+import java.util.Arrays;
+
+
+class Solution {
+    public boolean solve(int[] nums, int k) {
+        // Write your code here
+       int i=0,m=nums.length-1,ind=0;
+       Arrays.sort(nums);
+       while(i<m)
+       {
+           if(nums[i]+nums[m]<k)
+           {
+               i++;
+           }
+           else if(nums[i]+nums[m]>k)
+           {
+               m--;
+           }
+           else 
+           {
+               return true;
+           }
+       }
+       return false;
+       
+       
+    }
+}
